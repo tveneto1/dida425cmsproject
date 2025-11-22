cf6c396263b93f7d68f341075bb8f4e6beaf8fb2
tveneto1 <tveneto1@binghamton.edu>
2025-11-22 18:11:45 -0500

    restyled homepage based on in-class feedback

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import SignUpView
+from .views import SignUpView, logout_view
+from . import views
 
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path('logout/', views.logout_view, name='logout'), 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .forms import SignUpForm
+from django.contrib.auth import logout
 
 #login and signup
 class SignUpView(CreateView):
     form_class = SignUpForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
