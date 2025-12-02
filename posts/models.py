674e4245370bea0ee5398e568bf24b0549cd6c68
Lillian Pulaski <139179442+lillian-pulaski@users.noreply.github.com>
2025-12-01 22:01:11 -0500

    Add custom content uploading

--- posts/models.py
+++ posts/models.py
@@ -3,8 +3,9 @@ from django.core.validators import FileExtensionValidator
 from django.core.exceptions import ValidationError
 import magic
 
-#to check if the uploaded file is valid:
+# To check if the uploaded file is valid:
 ext_validator = FileExtensionValidator(['png', 'jpg', 'jpeg'])
+
 def validate_file_mimetype(file):
     accept = ['image/png', 'image/jpg', 'image/jpeg']
     file_mime_type = magic.from_buffer(file.read(1024), mime=True)
@@ -12,21 +13,29 @@ def validate_file_mimetype(file):
         raise ValidationError("Unsupported file type, only upload .png, .jpg, or .jpeg")
 
 
-
-# Create your models here.
-
 class Post(models.Model):
+    CONTENT_TYPE_CHOICES = [
+        ('image', 'Image Slide'),
+        ('custom', 'Custom Content'),
+    ]
+    
+    content_type = models.CharField(max_length=10, choices=CONTENT_TYPE_CHOICES, default='image')
     title = models.CharField(max_length=200)
-    cover = models.ImageField(upload_to='images/', validators=[ext_validator, validate_file_mimetype])
+    description = models.TextField(blank=True, null=True)
+    link = models.URLField(blank=True, null=True)
+    cover = models.ImageField(
+        upload_to='images/', 
+        validators=[ext_validator, validate_file_mimetype],
+        blank=True, 
+        null=True
+    )
     
     def __str__(self):
-        return self.title
-
-#overriding built in model delete
-#means that the deleted file also gets deleted from images folder within project -> but it isnt
-# ^ doesn't have to delete from folder, only from sqlite databsae which it does yay
+        return f"{self.get_content_type_display()} - {self.title}"
+    
+    # Overriding built-in model delete
+    # Means that the deleted file also gets deleted from images folder within project
     def delete(self, *args, **kwargs):
         if self.cover:
             self.cover.delete(save=False)
         super().delete(*args, **kwargs)
-
