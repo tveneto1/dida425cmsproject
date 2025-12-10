e55edc9d5f539114eac1540d9ef7923fda04b4e9
tveneto1 <tveneto1@binghamton.edu>
2025-12-10 13:16:51 -0500

    Update celery.py

--- cms425veneto/celery.py
+++ cms425veneto/celery.py
@@ -2,10 +2,10 @@ import os
 from celery import Celery
 
 # Set default Django settings module
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cms_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cms425veneto.settings")
 
 # Create Celery app instance
-app = Celery("cms_project")
+app = Celery("cms425veneto")
 
 # Load Celery settings from Django settings.py using the CELERY namespace
 app.config_from_object("django.conf:settings", namespace="CELERY")
